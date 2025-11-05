37d35710294f0f64ddba869251183354053d55f6
mkvbs <maciej.k-2000@outlook.com>
2025-11-05 18:12:42 +0100

    Initializtion of service

---
+++ src/main/kotlin/com/mkvbs/recipe_search_service/RecipeSearchServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.mkvbs.recipe_search_service
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RecipeSearchServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<RecipeSearchServiceApplication>(*args)
+}
---
+++ src/test/kotlin/com/mkvbs/recipe_search_service/RecipeSearchServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mkvbs.recipe_search_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RecipeSearchServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
